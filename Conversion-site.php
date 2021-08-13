aa4198322478385ed4cf3f35465d836c3c9b32e7
sudiptamani12 <85404404+sudiptamani12@users.noreply.github.com>
2021-08-13 23:10:57 +0600

    Add files via upload

---
+++ Conversion-site.php
@@ -0,0 +1,55 @@
+	<!doctype html> 
+	<html>
+	<head>
+		<meta charset ="utf-8">
+		<title>Converter</title>
+		<style >
+		#abc{
+			color: red;
+		}
+		#head1{
+			color: red;
+		}
+			
+		
+
+
+		</style>
+		#abd
+		{
+			1 feet = 12"
+		}
+	
+	</head>
+	<div style= row="5" cols="10"> 
+	
+		<fieldset>
+			<p id="head1"> Page 1: [Home]</p>
+
+		<p id="head1"> Conversion site</p>
+		
+
+		
+	<label for="converter" id="abc">Converter: </label>
+	<br>
+	<select id="converter" name="converter"> 
+	<!--span style="color: red"><?php echo $religionErr; ?></span-->
+			<option value="feet to inch" id="abd">feet to inch</option>
+			<option value="inch to feet">inch to feet</option>
+			
+		</select>
+		<br><br>
+		<label for="comment">Value :</label>
+			<textarea id="comment" name="comment" rows="2" cols="8"></textarea>
+			<br>
+			<br>
+			<label for="comment1">Result :</label>
+			<textarea id="comment" name="comment" rows="2" cols="8"></textarea>
+			
+
+</fieldset>
+
+
+</div>
+</body>
+</html>
