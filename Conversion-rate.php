337d63698bf3e1920188732dcac5f81f456953a9
sudiptamani12 <85404404+sudiptamani12@users.noreply.github.com>
2021-08-13 23:11:24 +0600

    Add files via upload

---
+++ Conversion-rate.php
@@ -0,0 +1,52 @@
+<!doctype html>   
+	<html>
+	<head>
+		<meta charset ="utf-8">
+		<title>Converter</title>
+		<style >
+		#abc{
+			color: red;
+		}
+		#head1{
+			color: red;
+		}
+			
+		</style>
+		
+	
+	<body>
+		
+	<div style = "position: absolute; top: 50%; left: 50%; transform: translate(-49%, -49%);"> 
+	
+	
+		<fieldset>
+			<p id="head1"> Page 1: [Conversion Site]</p>
+		<p id="head1"> Conversion site</p>
+		 <a href="http://localhost/a1/Home.php" target="_blank">Home</a>
+		  <a href="http://localhost/a1/conversion_rate.php" target="_blank">Converstion Rate</a>
+		  <a href="http://localhost/a1/History.php" target="_blank">History</a>
+		  <br> <br>
+		  
+		
+		
+	
+	<label for="converter" id="abc">Conversion rate: </label>
+	<br>
+	<select id="converter" name="converter"> 
+	
+			<option value="feet to inch">feet to inch</option>
+			<option value="inch to feet">inch to feet</option>
+			
+		</select>
+			<textarea id="comment" name="comment" rows="2" cols="8"></textarea>
+			
+			
+			<textarea id="comment" name="comment" rows="2" cols="8"></textarea>
+			<br>
+			<input type="Submit" value="submit">
+			
+			
+</fieldset>
+</div>
+</body>
+</html>
